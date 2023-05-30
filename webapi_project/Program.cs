d07a5f0d352843f69ad8225c1e7fc8873b52feab
Achileas <achigalatis@hotmail.com>
2023-05-31 02:55:40 +0300

    added swagger, added basic routes for testing

--- webapi_project/Program.cs
+++ webapi_project/Program.cs
@@ -1,14 +1,39 @@
+using System;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCors(options => {});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo { Title = "Note App", Description = "Take your note in markdown format", Version = "v1" }
+    );
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors("Cors value");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Note App v1");
+});
 
-app.MapGet("/api/note", () => "Hello, world");
+app.MapGet("/", () => "Hello, world");
+app.MapGet("/note", () =>
+{
+    return new[] { new Note("Note 1"), new Note("Note 2"), new Note("Note 3") };
+});
+
+app.MapPost("/note", (Note note) =>
+{
+    System.Console.WriteLine("{0}", note);
+});
 
 
 app.Run();
+
+public record Note(string note);
